e5587ab23072bb43b76e3c5b6ef0a36cf44aaf40
Hendrik Leppkes <h.leppkes@gmail.com>
2007-11-18 11:38:46 +0000

    initial alpha commit - very simple map mod

---
+++ Mapster.lua
@@ -0,0 +1,90 @@
+
+local db
+local defaults = {
+	profile = {
+		scale = 0.75,
+		alpha = 1,
+		strata = "FULLSCREEN"
+	}
+}
+
+local wmfOnShow, wmfStartMoving, wmfStopMoving
+
+local Mapster = LibStub("AceAddon-3.0"):NewAddon("Mapster", "AceEvent-3.0")
+
+function Mapster:OnInitialize()
+	self.db = LibStub("AceDB-3.0"):New("MapsterDB", defaults)
+	db = self.db.profile
+end
+
+local oldUIPanel, oldwmfOnKeyDown
+function Mapster:OnEnable()
+	oldUIPanel = UIPanelWindows["WorldMapFrame"]
+	UIPanelWindows["WorldMapFrame"] = nil
+	WorldMapFrame:SetAttribute("UIPanelLayout-enabled", false)
+	WorldMapFrame:HookScript("OnShow", wmfOnShow)
+	BlackoutWorld:Hide()
+	
+	oldwmfOnKeyDown = WorldMapFrame:GetScript("OnKeyDown")
+	WorldMapFrame:SetScript("OnKeyDown", nil)
+	
+	WorldMapFrame:SetMovable(true)
+	WorldMapFrame:RegisterForDrag("LeftButton")
+	WorldMapFrame:SetScript("OnDragStart", wmfStartMoving)
+	WorldMapFrame:SetScript("OnDragStop", wmfStopMoving)
+	
+	WorldMapFrame:ClearAllPoints()
+	WorldMapFrame:SetPoint("CENTER", UIParent, "CENTER", db.x or 0, db.y or 0)
+	
+	self:SetAlpha()
+	self:SetStrata()
+	
+	hooksecurefunc(WorldMapTooltip, "Show", function(self)
+		self:SetFrameStrata("TOOLTIP")
+	end)
+end
+
+function Mapster:OnDisable()
+	UIPanelWindows["WorldMapFrame"] = oldUIPanel
+	WorldMapFrame:SetAttribute("UIPanelLayout-enabled", true)
+	WorldMapFrame:SetScript("OnKeyDown", oldwmfOnKeyDown)
+	BlackoutWorld:Show()
+end
+
+function wmfOnShow(frame)
+	frame:SetScale(db.scale)
+	frame:SetWidth(1024)
+	frame:SetHeight(768)
+	Mapster:SetStrata()
+end
+
+function wmfStartMoving(frame)
+	frame:SetWidth(1024)
+	frame:SetHeight(768)
+	frame:StartMoving()
+end
+
+function wmfStopMoving(frame)
+	frame:StopMovingOrSizing()
+	
+	frame:SetWidth(1024)
+	frame:SetHeight(768)
+	-- save position relative to center of the screen
+	local x,y = frame:GetCenter()
+	local z = UIParent:GetEffectiveScale() / 2 / frame:GetScale()
+	db.x = x - GetScreenWidth() * z
+	db.y = y - GetScreenHeight() * z
+	frame:ClearAllPoints()
+	frame:SetPoint("CENTER", "UIParent", "CENTER", x, y)
+end
+
+function Mapster:SetStrata(value)
+	if value then db.strata = value end
+	WorldMapFrame:SetFrameStrata(db.strata)
+	WorldMapDetailFrame:SetFrameStrata(db.strata)
+end
+
+function Mapster:SetAlpha(value)
+	if value then db.alpha = value end
+	WorldMapFrame:SetAlpha(db.alpha)
+end
