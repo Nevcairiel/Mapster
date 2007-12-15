d9b1927f427bccd72e07ab928d3a3619a9ccbe07
Hendrik Leppkes <h.leppkes@gmail.com>
2007-12-15 16:22:11 +0000

    GroupIcons: use an OnUpdate schedule for updating the units

--- GroupIcons.lua
+++ GroupIcons.lua
@@ -10,23 +10,14 @@ local _G = _G
 
 local path = "Interface\\AddOns\\Mapster\\Artwork\\"
 
-local FixUnit, FixWorldMapUnits, FixBattlefieldUnits, UpdateUnitIcon
+local FixUnit, FixWorldMapUnits, FixBattlefieldUnits, OnUpdate, UpdateUnitIcon
 
 function GroupIcons:OnEnable()
-	self:RegisterEvent("PARTY_MEMBERS_UPDATE", "Update")
-	self:RegisterEvent("RAID_ROSTER_UPDATE", "Update")
 	self:RegisterEvent("ADDON_LOADED", function(event, addon)
 		if addon == "Blizzard_BattlefieldMinimap" then
 			FixBattlefieldUnits(true)
-			GroupIcons:Update()
 		end
-	end)
-	self:RegisterEvent("WORLD_MAP_UPDATE", function()
-		if not GroupIcons.timer then
-			GroupIcons.timer = GroupIcons:ScheduleRepeatingTimer("Update", 0.5)
-		end
-	end)
-	
+	end)	
 	FixWorldMapUnits(true)
 	FixBattlefieldUnits(true)
 end
@@ -40,7 +31,7 @@ function FixUnit(unit, state, isNormal)
 	local frame = _G[unit]
 	local icon = _G[unit.."Icon"]
 	if state then
-		frame:SetScript("OnUpdate", nil)
+		frame:SetScript("OnUpdate", OnUpdate)
 		if isNormal then
 			icon:SetTexture(path .. "Normal")
 		end
@@ -68,6 +59,18 @@ function FixBattlefieldUnits(state)
 	end
 end
 
+
+function OnUpdate(self)
+	local flash = GetTime() % 1 < 0.5
+	
+	local name = self:GetName().."Icon"
+	local texture = _G[name]
+	if texture then
+		local isRaid = strmatch(name, "Raid")
+		UpdateUnitIcon(texture, self.unit, flash, isRaid)
+	end
+end
+
 function UpdateUnitIcon(tex, unit, flash, isRaid)
 	local _, fileName = UnitClass(unit)
 	if not fileName then return end
@@ -92,41 +95,5 @@ function UpdateUnitIcon(tex, unit, flash, isRaid)
 		tex:SetVertexColor(t.r, t.g, t.b)
 	else --fallback grey, you never know what happens
 		tex:SetVertexColor(0.8, 0.8, 0.8)
-	end
-end
-
-function GroupIcons:Update()
-	local worldMapShown = WorldMapFrame:IsShown()
-	local battlefieldMinimapShown = BattlefieldMinimap and BattlefieldMinimap:IsVisible()
-	if not (worldMapShown or battlefieldMinimapShown) then
-		self:CancelTimer(self.timer)
-		self.timer = nil
-		return
-	end
-	
-	local flash = GetTime() % 1 < 0.5
-	
-	local numRaid = GetNumRaidMembers()
-	if numRaid > 0 then
-		for i = 1, numRaid do
-			local wmUnit = _G[fmt("WorldMapRaid%d", i)].unit
-			local bmUnit = battlefieldMinimapShown and _G[fmt("BattlefieldMinimapRaid%d", i)].unit
-			if not (wmUnit or bmUnit) then break end
-			-- update the world map units
-			if worldMapShown and wmUnit then
-				local tex = _G[fmt("WorldMapRaid%dIcon", i)]
-				UpdateUnitIcon(tex, wmUnit, flash, true)
-			end
-			-- update the battlefield minimap units
-			if bmUnit then
-				local tex = _G[fmt("BattlefieldMinimapRaid%dIcon", i)]
-				UpdateUnitIcon(tex, bmUnit, flash, true)
-			end
-		end
-	else
-		for i = 1, GetNumPartyMembers() do
-			local tex = _G[fmt("WorldMapParty%dIcon", i)]
-			UpdateUnitIcon(tex, fmt("party%d", i), flash)
-		end
 	end
 end
