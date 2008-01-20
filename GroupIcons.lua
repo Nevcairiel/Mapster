a19ca683553136dc529d6da0ba430901bd387db3
Hendrik Leppkes <h.leppkes@gmail.com>
2008-01-20 15:13:17 +0000

    GroupIcons: make groupicons work on the mini zone map if not in raids

--- GroupIcons.lua
+++ GroupIcons.lua
@@ -64,6 +64,9 @@ end
 
 function FixBattlefieldUnits(state)
 	if BattlefieldMinimap then
+		for i = 1, 4 do
+		FixUnit(fmt("BattlefieldMinimapParty%d", i), state, true)
+	end
 		for i = 1, 40 do
 			FixUnit(fmt("BattlefieldMinimapRaid%d", i), state)
 		end
