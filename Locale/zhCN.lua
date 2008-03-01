7d40f5888b3c4c34c01e92c6b5934e29d052418e
Hendrik Leppkes <h.leppkes@gmail.com>
2008-03-01 14:23:39 +0000

    remove zhCN locale again and add a notice to all locale files about UTF8 (plus one "control char" in UTF8 so anyone knows when its broken)

--- Locale/zhCN.lua
+++ Locale/zhCN.lua
@@ -1,34 +1,34 @@
 --[[ $Id$ ]]
+-- Please make sure to save the file as UTF-8; 露
 local L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "zhCN")
 if not L then return end
 
 -- Mapster Config
-L["intro_desc"] = "Mapster允许你控制世界地图的各种样式. 你可以改变地图的样式, 通过扩展插件来新增地图功能,并可以在每个人物下设置不同的配置."
-L["Style"] = "样式"
-L["alpha_desc"] = "改变世界地图的透明度, 使你打开地图的情况下继续操作你的人物."
-L["Alpha"] = "透明度"
-L["scale_desc"] = "调整世界地图的大小.它可以让你的世界地图不再覆盖整个屏幕."
-L["Scale"] = "缩放"
+-- L["intro_desc"] = "Mapster allows you to control various aspects of your World Map. You can change the style of the map, control the plugins that extend the map with new functionality, and configure different profiles for every of your characters."
+-- L["Style"] = true
+-- L["alpha_desc"] = "You can change the transparency of the world map to allow you to continue seeing the world environment while your map is open for navigation."
+-- L["Alpha"] = true
+-- L["scale_desc"] = "Change the scale of the world map if you do not want the whole screen filled while the map is open."
+-- L["Scale"] = true
 
 -- Coords
-L["Coordinates"] = "坐标" -- name of the module
-L["coords_desc"] = "在世界地图上增加你当前位置坐标和鼠标位置坐标."
-L["Enable Coordinates"] = "启用坐标"
-L["Accuracy"] = "精准"
-L["coords_accuracy_desc"] = "\n那你的坐标显示的更精确.例如:如果你需要精确的位置坐标,那么请设置到2."
+-- L["Coordinates"] = true -- name of the module
+-- L["coords_desc"] = "The Coordinates module adds a display of your current location, and the coordinates of your mouse cursor to the World Map frame."
+-- L["Enable Coordinates"] = true
+-- L["Accuracy"] = true
+-- L["coords_accuracy_desc"] = "\nYou can control the accuracy of the coordinates, e.g. if you need very exact coordinates you can set this to 2."
 
-L["Cursor"] = "坐标"
-L["Player"] = "玩家"
+-- L["Cursor"] = true
+-- L["Player"] = true
 
 -- Group Icons
-L["Group Icons"] = "队伍标记" -- name of the module
-L["groupicons_desc"] = "在世界地图和区域/战场地图上增加功能更强大的标记.它们可以详细的显示出职业以及队伍"
-L["Enable Group Icons"] = "启用队伍标记"
+-- L["Group Icons"] = true -- name of the module
+-- L["groupicons_desc"] = "The Group Icons module converts the player icons on the World Map and the Zone/Battlefield map to more meaningful icons, showing their class and (in raids) their sub-group."
+-- L["Enable Group Icons"] = true
 
 -- FogClear
-L["FogClear"] = "地图全亮" -- name of the module
-L["fogclear_desc"] = "地图全亮,显示你未征途的区域."
-L["Enable FogClear"] = "启用地图全亮"
-L["fogclear_desc_color"] = "如果你想知道未去过的区域, 那么你可以设置一层颜色来覆盖此区域的地图."
-L["Overlay Color"] = "覆盖颜色"
---by wolftankk@cwdg
+-- L["FogClear"] = true -- name of the module
+-- L["fogclear_desc"] = "The FogClear module removes the Fog of War from the World map, thus displaying the artwork for all the undiscovered zones."
+-- L["Enable FogClear"] = true
+-- L["fogclear_desc_color"] = "However, if you like to know which areas you did not discover yet, you can set a color that gets applied to those areas, so you notice where to go."
+-- L["Overlay Color"] = true
