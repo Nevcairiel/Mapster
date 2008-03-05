7b8f7ae86b65d9175a939d03744d6a6176fa0e0e
Hendrik Leppkes <h.leppkes@gmail.com>
2008-03-05 09:24:55 +0000

    zhTW locale from NightOwl on the forums

--- Locale/zhTW.lua
+++ Locale/zhTW.lua
@@ -4,31 +4,32 @@ local L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "zhTW")
 if not L then return end
 
 -- Mapster Config
--- L["intro_desc"] = "Mapster allows you to control various aspects of your World Map. You can change the style of the map, control the plugins that extend the map with new functionality, and configure different profiles for every of your characters."
--- L["Style"] = true
--- L["alpha_desc"] = "You can change the transparency of the world map to allow you to continue seeing the world environment while your map is open for navigation."
--- L["Alpha"] = true
--- L["scale_desc"] = "Change the scale of the world map if you do not want the whole screen filled while the map is open."
--- L["Scale"] = true
+ L["intro_desc"] = "Mapster讓你改變世界地圖的各種樣式. 通過擴展插件來新增地圖功能,並可以為每個角色設置不同的配置."
+ L["Style"] = "樣式"
+ L["alpha_desc"] = "改變世界地圖的透明度, 使你在打開地圖的情況下繼續操作你的人物."
+ L["Alpha"] = "透明度"
+ L["scale_desc"] = "調整世界地圖的大小.它可以讓你的世界地圖不再覆蓋整個螢幕."
+ L["Scale"] = "縮放"
 
 -- Coords
--- L["Coordinates"] = true -- name of the module
--- L["coords_desc"] = "The Coordinates module adds a display of your current location, and the coordinates of your mouse cursor to the World Map frame."
--- L["Enable Coordinates"] = true
--- L["Accuracy"] = true
--- L["coords_accuracy_desc"] = "\nYou can control the accuracy of the coordinates, e.g. if you need very exact coordinates you can set this to 2."
+ L["Coordinates"] = "座標" -- name of the module
+ L["coords_desc"] = "在世界地圖上加入你當前位置座標和滑鼠位置座標."
+ L["Enable Coordinates"] = "啟用座標"
+ L["Accuracy"] = "精准"
+ L["coords_accuracy_desc"] = "\n那你的座標顯示的更精確.例如:如果你需要精確的位置座標,那麼請設置到2."
 
--- L["Cursor"] = true
--- L["Player"] = true
+ L["Cursor"] = "鼠標"
+ L["Player"] = "玩家"
 
 -- Group Icons
--- L["Group Icons"] = true -- name of the module
--- L["groupicons_desc"] = "The Group Icons module converts the player icons on the World Map and the Zone/Battlefield map to more meaningful icons, showing their class and (in raids) their sub-group."
--- L["Enable Group Icons"] = true
+ L["Group Icons"] = "隊伍標記" -- name of the module
+ L["groupicons_desc"] = "在世界地圖和區域/戰場地圖上增加功能更強大的標記.它們可以詳細的顯示出職業以及隊伍"
+ L["Enable Group Icons"] = "啟用隊伍標記"
 
 -- FogClear
--- L["FogClear"] = true -- name of the module
--- L["fogclear_desc"] = "The FogClear module removes the Fog of War from the World map, thus displaying the artwork for all the undiscovered zones."
--- L["Enable FogClear"] = true
--- L["fogclear_desc_color"] = "However, if you like to know which areas you did not discover yet, you can set a color that gets applied to those areas, so you notice where to go."
--- L["Overlay Color"] = true
+ L["FogClear"] = "地圖全亮" -- name of the module
+ L["fogclear_desc"] = "地圖全亮,顯示你沒去過的區域."
+ L["Enable FogClear"] = "啟用地圖全亮"
+ L["fogclear_desc_color"] = "設置一層顏色來覆蓋你沒去過的區域地圖."
+ L["Overlay Color"] = "覆蓋顏色"
+--translated from the reference of  cwdg's zhCH translation, there is something call "change simplify chinese to traditional chinese tools ":P
