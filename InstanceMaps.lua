77b1f8119fe4becdfa9f32cd1e52345115555302
Hendrik Leppkes <h.leppkes@gmail.com>
2009-03-23 15:23:54 +0100

    Use a more conservative hooking that should not interfer with other addons

--- InstanceMaps.lua
+++ InstanceMaps.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright (c) 2008, Hendrik "Nevcairiel" Leppkes < h.leppkes@gmail.com >
+Copyright (c) 2009, Hendrik "Nevcairiel" Leppkes < h.leppkes@gmail.com >
 All rights reserved.
 ]]
 
@@ -11,7 +11,7 @@ local Maps = Mapster:NewModule(MODNAME, "AceHook-3.0")
 
 local data = {
 	-- Northrend Instances
-	[5] = {
+	{
 		["Ahn'kahet: The Old Kingdom"] = {
 			"AhnKahet",
 			{"Area 1", "Area 2"},
@@ -70,7 +70,7 @@ local data = {
 	},
 
 	-- Northrend Raids
-	[6] = {
+	{
 		["Naxxramas"] = {
 			"Naxxramas",
 			{"The Construct Quarter", "The Arachnid Quarter", "The Military Quarter", "The Plague Quarter", "Naxxramas Overview", "Sapphiron and Kel'Thuzad"},
@@ -80,7 +80,7 @@ local data = {
 			"EyeOfEternity",
 			{"Area 1"},
 		},
-		["The Obsidian Sanctum"] = "TheObsidianSanctum\\TheObsidianSanctum%d",
+		["The Obsidian Sanctum"] = "TheObsidianSanctum",
 --		["The Obsidian Sanctum"] = {
 --			"TheObsidianSanctum",
 --			{"Area 1"},
@@ -94,7 +94,7 @@ local data = {
 			{"Area 1"},
 		},
 	},
-	[7] = {
+	{
 		["Alterac Valley"] = "AlteracValley",
 		["Arathi Basin"] = "ArathiBasin",
 		["Eye of the Storm"] = "NetherstormArena",
@@ -132,6 +132,8 @@ local function getOptions()
 	return options
 end
 
+local cont_offset
+
 function Maps:OnInitialize()
 	self.db = Mapster.db:RegisterNamespace(MODNAME, defaults)
 	db = self.db.profile
@@ -139,9 +141,12 @@ function Maps:OnInitialize()
 	self:SetEnabledState(Mapster:GetModuleEnabled(MODNAME))
 	Mapster:RegisterModuleOptions(MODNAME, getOptions, L["Instance Maps"])
 
+	cont_offset = select('#', GetMapContinents())
+
 	self.zones = {}
 
-	for id, data in pairs(data) do
+	for i, data in pairs(data) do
+		local id = i + cont_offset
 		self.zones[id] = {}
 		for name in pairs(data) do
 			-- Todo: translate with babble-zone here, maybe?
@@ -152,84 +157,148 @@ function Maps:OnInitialize()
 end
 
 function Maps:OnEnable()
-	self:RawHook("GetMapContinents", true)
-	self:RawHook("GetCurrentMapContinent", true)
+	self:RawHook("WorldMapContinentsDropDown_Update", true)
+	self:RawHook("WorldMapFrame_LoadContinents", true)
 
-	self:RawHook("GetMapZones", true)
-	self:RawHook("GetCurrentMapZone", true)
+	self:RawHook("WorldMapZoneDropDown_Update", true)
+	self:RawHook("WorldMapZoneDropDown_Initialize", true)
+	self:RawHook("WorldMapZoneButton_OnClick", true)
 
-	self:RawHook("GetNumDungeonMapLevels", true)
-	self:RawHook("GetCurrentMapDungeonLevel", true)
+	self:RawHook("WorldMapLevelDropDown_Update", true)
+	self:RawHook("WorldMapLevelDropDown_Initialize", true)
+	WorldMapLevelUpButton:SetScript("OnClick", self.WorldMapLevelUp_OnClick)
+	WorldMapLevelDownButton:SetScript("OnClick", self.WorldMapLevelDown_OnClick)
 
 	self:RawHook("SetMapZoom", true)
 	self:RawHook("SetDungeonMapLevel", true)
 	self:Hook("SetMapToCurrentZone", true)
 
-	self:RawHook("WorldMapZoomOutButton_OnClick", true)
-
-	self:RawHook("GetMapInfo", true)
+	self:RawHook("WorldMapFrame_Update", true)
 end
 
 function Maps:OnDisable()
+	self:UnhookAll()
+	self.mapCont, self.mapZone, self.dungeonLevel = nil, nil, nil
+	WorldMapFrame_Update()
+	WorldMapContinentsDropDown_Update()
+	WorldMapZoneDropDown_Update()
+	WorldMapLevelDropDown_Update()
 
+	WorldMapLevelUpButton:SetScript("OnClick", WorldMapLevelUp_OnClick)
+	WorldMapLevelDownButton:SetScript("OnClick", WorldMapLevelDown_OnClick)
 end
 
-function Maps:GetMapContinents()
-	local continents = {self.hooks.GetMapContinents()}
-	tinsert(continents, L["Northrend Instances"])
-	tinsert(continents, L["Northrend Raids"])
-	tinsert(continents, L["Battlegrounds"])
-	return unpack(continents)
-end
-
-function Maps:GetCurrentMapContinent()
-	return self.mapCont or self.hooks.GetCurrentMapContinent()
-end
-
-function Maps:GetMapZones(continent)
-	if self.zones[continent] then
-		return unpack(self.zones[continent])
+function Maps:WorldMapContinentsDropDown_Update()
+	self.hooks.WorldMapContinentsDropDown_Update()
+	if self.mapCont then
+		UIDropDownMenu_SetSelectedID(WorldMapContinentDropDown, self.mapCont)
 	end
-	return self.hooks.GetMapZones(continent)
 end
 
-function Maps:GetCurrentMapZone()
-	return self.mapCont and (self.mapZone or 0) or self.hooks.GetCurrentMapZone()
+function Maps:WorldMapFrame_LoadContinents(...)
+	self.hooks.WorldMapFrame_LoadContinents(...)
+
+	local info = UIDropDownMenu_CreateInfo()
+	info.text =  L["Northrend Instances"]
+	info.func = WorldMapContinentButton_OnClick;
+	info.checked = nil;
+	UIDropDownMenu_AddButton(info)
+
+	info.text =  L["Northrend Raids"]
+	info.func = WorldMapContinentButton_OnClick;
+	info.checked = nil;
+	UIDropDownMenu_AddButton(info)
+
+	info.text =  L["Battlegrounds"]
+	info.func = WorldMapContinentButton_OnClick;
+	info.checked = nil;
+	UIDropDownMenu_AddButton(info)
 end
 
-function Maps:GetNumDungeonMapLevels()
+function Maps:WorldMapZoneDropDown_Update()
+	self.hooks.WorldMapZoneDropDown_Update()
+	if self.mapZone then
+		UIDropDownMenu_SetSelectedID(WorldMapZoneDropDown, self.mapZone)
+	end
+end
+
+function Maps:WorldMapZoneDropDown_Initialize()
+	if self.mapCont then
+		WorldMapFrame_LoadZones(unpack(self.zones[self.mapCont]))
+	else
+		self.hooks.WorldMapZoneDropDown_Initialize()
+	end
+end
+
+function Maps:WorldMapZoneButton_OnClick(frame)
+	if self.mapCont then
+		UIDropDownMenu_SetSelectedID(WorldMapZoneDropDown, frame:GetID());
+		SetMapZoom(self.mapCont, frame:GetID());
+	else
+		self.hooks.WorldMapZoneButton_OnClick(frame)
+	end
+end
+
+function Maps:WorldMapLevelDropDown_Update()
+	self.hooks.WorldMapLevelDropDown_Update()
+	if self.mapCont and self.mapZone and self:GetNumDungeonMapLevels() > 0 then
+		UIDropDownMenu_SetSelectedID(WorldMapLevelDropDown, self.dungeonLevel)
+		WorldMapLevelDropDown:Show()
+		WorldMapLevelUpButton:Show()
+		WorldMapLevelDownButton:Show()
+	end
+end
+
+function Maps:WorldMapLevelDropDown_Initialize()
 	if self.mapCont and self.mapZone then
-		local zone_data = data[self.mapCont][self.zones[self.mapCont][self.mapZone]]
-		if type(zone_data) == "table" then
-			return #(zone_data[2])
-		else
-			return 0
+		local info = UIDropDownMenu_CreateInfo()
+		local level = self.dungeonLevel
+
+		local mapname = strupper(GetMapInfo() or "");
+
+		for i=1, self:GetNumDungeonMapLevels() do
+			local floorname =_G["DUNGEON_FLOOR_" .. mapname .. i];
+			info.text = floorname or string.format(FLOOR_NUMBER, i);
+			info.func = WorldMapLevelButton_OnClick;
+			info.checked = (i == level);
+			UIDropDownMenu_AddButton(info);
 		end
 	else
-		return self.hooks.GetNumDungeonMapLevels()
+		self.hooks.WorldMapLevelDropDown_Initialize()
 	end
 end
 
-function Maps:GetCurrentMapDungeonLevel()
-	return self.dungeonLevel or self.hooks.GetCurrentMapDungeonLevel()
+function Maps.WorldMapLevelUp_OnClick(frame)
+	if Maps.mapCont and Maps.mapZone then
+		Maps:SetDungeonMapLevel(Maps.dungeonLevel - 1)
+		UIDropDownMenu_SetSelectedID(WorldMapLevelDropDown, Maps.dungeonLevel)
+		PlaySound("UChatScrollButton")
+	else
+		WorldMapLevelUp_OnClick(frame)
+	end
+end
+
+function Maps.WorldMapLevelDown_OnClick(frame)
+	if Maps.mapCont and Maps.mapZone then
+		Maps:SetDungeonMapLevel(Maps.dungeonLevel + 1)
+		UIDropDownMenu_SetSelectedID(WorldMapLevelDropDown, Maps.dungeonLevel)
+		PlaySound("UChatScrollButton")
+	else
+		WorldMapLevelDown_OnClick(frame)
+	end
 end
 
 function Maps:SetMapZoom(cont, zone)
 	if self.zones[cont] then
 		self.mapCont = cont
 		self.mapZone = zone
-		cont = -1
-		zone = nil
-		self.hooks.SetMapZoom(-1)
-		if self.mapZone then
-			if GetNumDungeonMapLevels() > 0 then
-				self.dungeonLevel = 1
+		if zone then
+			if self:GetNumDungeonMapLevels() > 0 then
+				self.dungeonLevel = (self:GetNumDungeonMapLevels() > 0) and 1 or 0
 			end
-			WorldMapFrame_Update()
-			WorldMapLevelDropDown_Update()
-		else
-			WorldMapZoneDropDown_Update()
 		end
+		self:WorldMapFrame_Update()
+		self.hooks.SetMapZoom(-1)
 	else
 		self.mapCont = nil
 		self.mapZone = nil
@@ -238,10 +307,23 @@ function Maps:SetMapZoom(cont, zone)
 	end
 end
 
+function Maps:GetNumDungeonMapLevels()
+	if self.mapCont and self.mapZone then
+		local zone_data = data[self.mapCont - cont_offset][self.zones[self.mapCont][self.mapZone]]
+		if type(zone_data) == "table" then
+			return #(zone_data[2])
+		else
+			return 0
+		end
+	else
+		return GetNumDungeonMapLevels()
+	end
+end
+
 function Maps:SetDungeonMapLevel(level)
 	if self.mapCont and self.mapZone then
-		self.dungeonLevel = max(1, min(level, GetNumDungeonMapLevels()))
-		WorldMapFrame_Update()
+		self.dungeonLevel = max(1, min(level, self:GetNumDungeonMapLevels()))
+		self:WorldMapFrame_Update()
 	else
 		self.hooks.SetDungeonMapLevel(level)
 	end
@@ -251,27 +333,31 @@ function Maps:SetMapToCurrentZone()
 	self.mapCont, self.mapZone, self.dungeonLevel = nil, nil, nil
 end
 
-function Maps:WorldMapZoomOutButton_OnClick()
-	if self.mapZone then
-		self.mapCont, self.mapZone, self.dungeonLevel = nil, nil, nil
-		WorldMapFrame_Update()
-		WorldMapContinentsDropDown_Update()
-		WorldMapZoneDropDown_Update()
-		WorldMapLevelDropDown_Update()
+function Maps:WorldMapFrame_Update()
+	if self.mapCont and self.mapZone then
+		OutlandButton:Hide()
+		AzerothButton:Hide()
+
+		local mapFileName
+		local zone_data = data[self.mapCont-cont_offset][self.zones[self.mapCont][self.mapZone]]
+		if type(zone_data) == "table" then
+			mapFileName = zone_data[1]
+		else
+			mapFileName = zone_data
+		end
+
+		local texName
+		local dungeonLevel = self.dungeonLevel or 0
+		for i=1, NUM_WORLDMAP_DETAIL_TILES do
+			if ( dungeonLevel > 0 ) then
+				texName = "Interface\\WorldMap\\"..mapFileName.."\\"..mapFileName..dungeonLevel.."_"..i;
+			else
+				texName = "Interface\\WorldMap\\"..mapFileName.."\\"..mapFileName..i;
+			end
+			_G["WorldMapDetailTile"..i]:SetTexture(texName);
+		end
 	else
-		self.hooks.WorldMapZoomOutButton_OnClick()
+		self.hooks.WorldMapFrame_Update()
 	end
 end
 
-function Maps:GetMapInfo()
-	if self.mapCont and self.mapZone then
-		local zone_data = data[self.mapCont][self.zones[self.mapCont][self.mapZone]]
-		if type(zone_data) == "table" then
-			return zone_data[1]
-		else
-			return zone_data
-		end
-	else
-		return self.hooks.GetMapInfo()
-	end
-end
