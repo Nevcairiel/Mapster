ec48a8a80e574bdb084e49a6183dd457435a5b0f
Hendrik Leppkes <h.leppkes@gmail.com>
2012-07-11 22:47:35 +0200

    Add itIT locale

---
+++ Locale/itIT.lua
@@ -0,0 +1,8 @@
+-- Mapster Locale
+-- Please use the Localization App on WoWAce to Update this
+-- http://www.wowace.com/projects/mapster/localization/ ;¶
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Mapster", "itIT")
+if not L then return end
+
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="comment")@
